6ed7d983608396356c5f9c37b92a748ffe39e8bf
giupb <giulio.pbrugalli@gmail.com>
2023-06-23 10:32:52 -0300

    test YoutubeDownloadPiece

---
+++ tests/test_youtube_download_piece.py
@@ -0,0 +1,38 @@
+from domino.scripts.piece_dry_run import piece_dry_run
+
+def run_piece(
+        url: str,
+        output_type: str,
+):
+    
+    return piece_dry_run(    
+        #local piece repository path
+        repository_folder_path="../",
+
+        #name of the piece
+        piece_name="YoutubeDownloadPiece",
+
+        #values to the InputModel arguments
+        piece_input={
+            "url": url,
+            "output_type": output_type
+        },
+)
+
+
+def test_piece():
+    piece_kwargs = {
+        "url": "https://www.youtube.com/watch?v=zhWDdy_5v2w&ab_channel=AsapSCIENCE",
+        "output_type": "audio"
+    }
+    output = run_piece(
+        **piece_kwargs
+    )
+
+    if piece_kwargs["output_type"] == "audio":
+        assert output.file_path.endswith(".mp3")
+    if piece_kwargs["output_type"] == "video":
+        assert output.file_path.endswith(".mp4")
+
+if __name__ == "__main__":
+    test_piece()
